0e3ba47ba56b4d5cab9e21608b970ff67c0a7e3a
Samikmalhotra <samikmalhotra66@gmail.com>
2021-08-04 21:35:20 +0530

    added redirect

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import TextEditor from './components/TextEditor';
+import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom'
+import {v4 as uuidV4} from 'uuid'
 
 function App() {
   return (
-    <div className="App">
-      <TextEditor/>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Redirect to = {`/documents/${uuidV4()}`}/>
+        </Route>
+        <Route path='/documents/:id'><TextEditor/></Route>
+
+      </Switch>
+    </Router>
   );
 }
 
